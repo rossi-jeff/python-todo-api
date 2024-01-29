b12d50e808f0c2971a4df8b184fb22e6b658496b
Jeff Rossi <rossi.jeff@gmail.com>
2024-01-29 17:39:32 -0500

    restructure routes

--- app.py
+++ app.py
@@ -1,20 +1,7 @@
 from flask import Flask
-from flask_restx import  Api
-from auth import auth_ns
-from todo import todo_ns
-from user import user_ns
-
+from routes.api import api
 
 app = Flask(__name__)
-api = Api(
-    title="Todo API",
-    version="1.0",
-    description="ToDo with Authorization"
-)
-
-api.add_namespace(auth_ns)
-api.add_namespace(todo_ns)
-api.add_namespace(user_ns)
 
 api.init_app(app)
 
